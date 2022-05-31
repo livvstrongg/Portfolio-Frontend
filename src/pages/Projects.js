88d85ba860b9dc1ea656938826e4dc23d0c3627f
livvstrongg <livyhawk@me.com>
2022-05-31 14:40:31 -0500

    updates

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,39 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 function Projects(props) {
-  // create state to hold projects
-  const [projects, setProjects] = useState(null);
+    const [projects, setProjects] = useState(null);
 
-  //create function to make api call
-  const getProjectsData = async () => {
-    //make api call and get response
-    const response = await fetch(props.URL + "projects");
-    // turn response into javascript object
-    const data = await response.json();
-    // set the projects state to the data
-    setProjects(data);
-  };
+    useEffect(() => {
+        const getProjectsData = async () => {
+            const response = await fetch(props.URL + "projects");
+            const data = await response.json();
+            setProjects(data);
+        };
+        getProjectsData();
+    }, [props.URL]);
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
-
-  // define a function that will return the JSX needed once we get the data
-  const loaded = () => {
-    return projects.map((project) => (
-      <div>
-        <h1>{project.name}</h1>
-        <img src={project.image} />
-        <a href={project.git}>
-          <button>Github</button>
-        </a>
-        <a href={project.live}>
-          <button>live site</button>
-        </a>
-      </div>
-    ));
-  };
-
-  return projects ? loaded() : <h1>Loading...</h1>;
-}
+    const loaded = () => {
+        return projects.map((project) => (
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} alt='project' />
+                <a href={project.git}>
+                    <button>Github</button>
+                </a>
+                <a href={project.live}>
+                    <button>live site</button>
+                </a>
+            </div>
+        ));
+    };
+    return projects ? loaded() : <h1>Loading...</h1>;
+};
 
 export default Projects;
